eb3e522d6f35325451a90f86e89d9a382609ab0a
curiousBrahmin <Rambo@Rambos-iMac.hsd1.tn.comcast.net>
2016-09-10 10:34:36 -0500

    CAT YEARS

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var ageTextField: UITextField!
+   
+    @IBOutlet weak var ageCalulated: UILabel!
+    
+    
+    @IBAction func calculateBtnPressed(_ sender: AnyObject) {
+        
+        let ageEntered = ageTextField.text
+        let ageResult = Int(ageEntered!)! * 7
+        
+        ageCalulated.text = String(ageResult)
+        
+        
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
     }
-
 
 }
 
